9b3620381bd41fb0710a09f3e8b1f50ea333e84f
Alejo Gutierrez <alejo.gutierrez@itoaxaca.edu.mx>
2019-11-12 18:35:47 -0600

    Saved again!

--- config/services.php
+++ config/services.php
@@ -33,5 +33,11 @@ return [
     'sparkpost' => [
         'secret' => env('SPARKPOST_SECRET'),
     ],
+    
+    'facebook' => [
+        'client_id' => env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT'),
+      ],
 
 ];

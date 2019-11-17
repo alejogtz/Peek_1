a3fb9c2c1cd5cb28d57d6f32010b743601e581bd
Alejo Gutierrez <alejo.gutierrez@itoaxaca.edu.mx>
2019-11-17 15:46:12 -0600

    Se cambio la url de la app para funcionar en entorno Heroku

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tec-emergente-peek.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 

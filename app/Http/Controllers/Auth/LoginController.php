382622d13c010cc0e05333ff10e9a94560dfbb23
Aleko <alejox.gutierrez@gmail.com>
2019-11-19 20:23:35 -0600

    Google OAuth Reparado. TEST 3

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use App\Http\Controllers\Auth\SocialGoogleAccountService;
+use App\Services\SocialGoogleAccountService;
 
 
 use App\User;

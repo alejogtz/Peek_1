9a15aa549e2d574e9135f3df55f012d9b88c2068
System Administrator <root@MacBook-Pro-de-Jesus-2.local>
2019-11-30 23:23:09 -0600

    vistasactualizadas2

--- app/Models/SocialAccount.php
+++ app/Models/SocialAccount.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 class SocialAccount extends Model{
 	//aqui se declara el nombre de la tabla que esta en mysql
 	protected $table='social_accounts';
@@ -22,4 +23,10 @@ class SocialAccount extends Model{
 	
 	//aqui los elementos a mostrarse de la tabla en cuestion
 	protected $fillable=['user_id','provider_user_id','provider'];
+
+
+	public function user()
+  {
+      return $this->belongsTo(User::class);
+  }
 }

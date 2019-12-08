cb4877b44cc31d01725ac618386f010e1ab9b334
Miguel Angel Gomez Porras <37426099+GomezPorrasMiguelAngel@users.noreply.github.com>
2019-12-08 17:36:11 -0600

    Update FeedController.php
    Se agrego feed de prueba con un solo valor

--- app/Http/Controllers/Peek/FeedController.php
+++ app/Http/Controllers/Peek/FeedController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Peek;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Models\Post;
+use App\Models\Like;
+use App\Models\Comment;
 
 class FeedController extends Controller
 {
@@ -12,4 +16,13 @@ class FeedController extends Controller
         $posts = Post::orderBy('id', 'DESC')->get();
         return $posts;
 	}
+  public function feed2()
+{
+      $posts = Post::orderBy('id', 'DESC')->first(1);
+      $usuario = User::find($posts->get('user_id'));
+      $likes = Like::where('post_id','=',$posts->get('id'))->count();
+      //$comments = Comment::where('post_id','=',$posts->get('id'))->get(1);
+      $datos = ['posts' => $posts, 'usuario' => $usuario,'likes' => $likes];
+      return view('feed', compact('datos'));
+}
 }
